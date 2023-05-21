a10445536a9b536f05aa5fd1e5464b19d86b9761
kevinphong1203@gmail.com <kevinphong1203@gmail.com>
2023-05-21 08:28:30 +0700

    add project

--- BTVN-PHP/lesson-6/lesson-6.php
+++ BTVN-PHP/lesson-6/lesson-6.php
@@ -2,7 +2,46 @@
 // 
 
 class Calculator {
+    private $numbers;
+
+    public function __construct(){
+        $this -> numbers = array();
+    }
+    // them phan tu vao mang
+    public function addNumver ($number){
+        $this -> numbers[] = $number;
+    }
+    //ham in ra cac phan tu da nhap
+    public function inNumcers (){
+        echo "Number array: " . implode(", ", $this -> numbers). "<br>";
+    }
+    public function countArray (){
+        echo "Count Array: " .count($this -> numbers) . "<br>";
+    }
+    public function sumArray (){
+        echo " Sum of Array: " . array_sum($this-> numbers) . "<br>";
+    }
+    public function uniqeArray (){
+        echo " Uniqe of Array: " . implode(", ",array_unique($this -> numbers)) . "<br>";
+    }
     
 }
 
 $calc = new Calculator;
+// nhập phần tử trong mang
+$calc -> addNumver(5);
+$calc -> addNumver(9);
+$calc -> addNumver(7);
+$calc -> addNumver(2);
+$calc -> addNumver(1);
+$calc -> addNumver(9);
+$calc -> addNumver(2);
+
+// in ra mảng
+$calc -> inNumcers();
+// number in array
+$calc -> countArray();
+// Sum Array
+$calc -> sumArray();
+// Unique Array 
+$calc -> uniqeArray();
